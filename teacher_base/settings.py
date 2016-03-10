fecc77c9642cfa948c7b6f96b9e4100952bca5db
xlm <1158529273@qq.com>
2016-03-10 16:49:38 +0800

    xuanzheti

--- teacher_base/settings.py
+++ teacher_base/settings.py
@@ -86,3 +86,7 @@ TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
 TEMPLATE_DIRS = [
         TEMPLATE_PATH,
         ]
+STATIC_PATH = os.path.join(BASE_DIR, 'static' )
+STATICFILES_DIRS = (
+        STATIC_PATH,
+        ) 
